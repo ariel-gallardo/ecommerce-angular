feed8b8fb9076e8d6199444022fa57e02cdb3d10
Ariel Gallardo <ariel.gallardo.dev@gmail.com>
2025-12-23 09:45:32 -0300

    fix(policy-access): include action name when action label is empty

--- src/pipes/policy-access.pipe.ts
+++ src/pipes/policy-access.pipe.ts
@@ -15,7 +15,8 @@ export class PolicyAccessPipe implements PipeTransform {
     else if(permission.controller && permission.action)
     {
       const action = ACTION_CONFIG[permission.action as Action] ?? { label: '', color: Colors.Unknown, icon: 'api' };
-      return {...action, label: action.label != '' ? `${permission.controller} | ${action.label}` : permission.controller} 
+      return {...action, label: action.label != '' ? `${permission.controller} | ${action.label}` 
+      : `${permission.controller} | ${permission.action}`} 
     }
     else return { label: 'Desconocido', color: '#fff', icon: 'help' };
   }
